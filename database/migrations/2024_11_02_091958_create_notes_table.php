1393f829833cc6d4a725640ae1bf75b7dadf417b
Umayanga12 <umayangaathapaththu05@gmail.com>
2024-11-02 16:14:30 +0530

    db seeding and make note controller

--- database/migrations/2024_11_02_091958_create_notes_table.php
+++ database/migrations/2024_11_02_091958_create_notes_table.php
@@ -4,15 +4,16 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('notes', function (Blueprint $table) {
+        Schema::create("notes", function (Blueprint $table) {
             $table->id();
+            $table->longText("note");
+            $table->foreignId("user_id")->constrained("users");
             $table->timestamps();
         });
     }
@@ -22,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('notes');
+        Schema::dropIfExists("notes");
     }
 };
